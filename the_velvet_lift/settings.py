920c5a7b530ec76ef57aec1da346a5457c49a118
Katarina Kilian <khkilian22@gmail.com>
2025-02-08 10:08:49 +0000

    Debug inplace

--- the_velvet_lift/settings.py
+++ the_velvet_lift/settings.py
@@ -26,7 +26,7 @@ if os.path.exists("env.py"):
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "8000-kakilian-thevelvetlift-x7jyvlk7svi.ws.codeinstitute-ide.net",
